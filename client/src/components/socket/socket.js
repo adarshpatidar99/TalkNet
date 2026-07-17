4d3bdf73cb3d36c0dc79fdbc45447da47f7a6740
AdarshPatidar99 <adarshpatidar316@gmail.com>
2026-07-17 18:42:32 +0530

    Configure socket for production

--- client/src/components/socket/socket.js
+++ client/src/components/socket/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const socket = io(import.meta.env.VITE_BACKEND_URL, {
+const socket = io(import.meta.env.VITE_API_URL, {
   transports: ["websocket"],
   withCredentials: true,
   autoConnect: false,
